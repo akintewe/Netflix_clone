07fe5915d962fe02b7df4036f4c9d6b934d82b1b
Akintewe Oluwasola <nathan@Akintewes-MacBook-Pro.local>
2022-11-25 21:03:35 +0100

    first commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,6 +15,25 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Stack(
+                children: [
+                  Positioned(
+                      child: Container(
+                          width: Get.width,
+                          child: Image.asset(
+                            'assets/images/movie.png',
+                            fit: BoxFit.cover,
+                          )))
+                ],
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
