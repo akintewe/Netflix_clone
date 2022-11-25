12002a0c048978e2b7892ea8545f742f73ede571
Akintewe Oluwasola <nathan@Akintewes-MacBook-Pro.local>
2022-11-25 22:09:38 +0100

    still making changes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -15,144 +15,181 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Stack(
-                children: [
-                  Positioned(
-                      child: Container(
-                          width: Get.width,
-                          child: Image.asset(
-                            'assets/images/movie.png',
-                            fit: BoxFit.cover,
-                          ))),
-                  Positioned(
-                      top: 40,
-                      child: Container(
-                        child: Image.asset('assets/images/navbar.png'),
-                      ))
-                ],
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: Get.width * 0.06,
-                child: Image.asset(
-                  'assets/images/top10.png',
-                  fit: BoxFit.contain,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Positioned(
+                        child: Container(
+                            width: Get.width,
+                            child: Image.asset(
+                              'assets/images/movie.png',
+                              fit: BoxFit.cover,
+                            ))),
+                    Positioned(
+                        top: 40,
+                        child: Container(
+                          child: Image.asset('assets/images/navbar.png'),
+                        ))
+                  ],
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: Get.width * 0.06,
+                  child: Image.asset(
+                    'assets/images/top10.png',
+                    fit: BoxFit.contain,
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: Get.width * 0.02,
-              ),
-              Text(
-                '#2 in Nigeria Today',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 16,
+                SizedBox(
+                  width: Get.width * 0.02,
                 ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: Get.height * 0.01,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [
-                  IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.add,
+                Text(
+                  '#2 in Nigeria Today',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: Get.height * 0.01,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 35,
+                        )),
+                    Text(
+                      'My List',
+                      style: TextStyle(
                         color: Colors.white,
-                        size: 35,
-                      )),
-                  Text(
-                    'My List',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )
-                ],
-              ),
-              Container(
-                  height: Get.height * 0.08,
-                  width: Get.width * 0.38,
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(196, 196, 196, 1),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Stack(
-                    children: [
-                      Positioned(
-                          bottom: 20,
-                          child: IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.play_arrow,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                    height: Get.height * 0.08,
+                    width: Get.width * 0.38,
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(196, 196, 196, 1),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                            bottom: 20,
+                            child: IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.play_arrow,
+                                  color: Colors.black,
+                                  size: 60,
+                                ))),
+                        Positioned(
+                            bottom: 15,
+                            right: 20,
+                            child: Text(
+                              'Play',
+                              style: TextStyle(
                                 color: Colors.black,
-                                size: 60,
-                              ))),
-                      Positioned(
-                          bottom: 15,
-                          right: 20,
-                          child: Text(
-                            'Play',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ))
-                    ],
-                  )),
-              Column(
-                children: [
-                  IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.info_outline_rounded,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ))
+                      ],
+                    )),
+                Column(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.info_outline_rounded,
+                          color: Colors.white,
+                          size: 35,
+                        )),
+                    Text(
+                      'Info',
+                      style: TextStyle(
                         color: Colors.white,
-                        size: 35,
-                      )),
-                  Text(
-                    'Info',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: [
-              SizedBox(
-                width: 10,
-              ),
-              Text(
-                'Previews',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 10,
                 ),
-              )
-            ],
-          )
-        ],
+                Text(
+                  'Previews',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                )
+              ],
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    height: Get.height * 0.21,
+                    width: Get.width * 0.4,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(100)),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    height: Get.height * 0.21,
+                    width: Get.width * 0.4,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(100)),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    height: Get.height * 0.21,
+                    width: Get.width * 0.4,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(100)),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
